ad277d6d5bef95133f737cfdd5d8f6b649d569fa
YifeiGong <yifeiaimeegong@hotmail.com>
2019-11-01 09:32:20 -0500

    setup

---
+++ build/precache-manifest.e33ab5d018628acd9a2361ffee98383e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14bfc452ac90eb410fffc982c2b11d9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e11dfba5688b0623ffb",
+    "url": "/static/css/2.58bd2952.chunk.css"
+  },
+  {
+    "revision": "32f49b2ff37493604fc1",
+    "url": "/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "3e11dfba5688b0623ffb",
+    "url": "/static/js/2.0a483d86.chunk.js"
+  },
+  {
+    "revision": "32f49b2ff37493604fc1",
+    "url": "/static/js/main.52b4e3b3.chunk.js"
+  },
+  {
+    "revision": "1c39347ee83a41df164e",
+    "url": "/static/js/runtime-main.8fbfdb02.js"
+  }
+]);
